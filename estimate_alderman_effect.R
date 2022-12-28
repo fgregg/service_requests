6fabb134470936f612b899d4b7018cbcc2957845
Forest Gregg <fgregg@datamade.us>
2022-12-27 21:12:54 -0500

    really drop the items that are closed within one day

--- estimate_alderman_effect.R
+++ estimate_alderman_effect.R
@@ -6,7 +6,7 @@ service_requests$WARD = as.factor(service_requests$WARD)
 service_requests$SR_TYPE = as.factor(service_requests$SR_TYPE)
 
 completed_service_requests = service_requests[service_requests$completed == 1, ]
-not_immediately_closed = completed_service_requests[completed_service_requests$time_to_completion > 0, ]
+not_immediately_closed = completed_service_requests[completed_service_requests$time_to_completion >= 1, ]
 
 model = lm(log(time_to_completion) ~ 1 + SR_TYPE + WARD * aldermanic_request,
            not_immediately_closed)
